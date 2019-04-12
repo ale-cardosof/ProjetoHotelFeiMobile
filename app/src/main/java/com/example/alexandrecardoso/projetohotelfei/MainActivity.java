5024d8888f686da927205d7a51870b7e9d2ad52c
Alexandre Cardoso <alexandrecardosofeitosa@outlook.com.br>
2019-04-11 23:17:30 -0300

    Leve Alteração na criação do Vetor Adm.

--- app/src/main/java/com/example/alexandrecardoso/projetohotelfei/MainActivity.java
+++ app/src/main/java/com/example/alexandrecardoso/projetohotelfei/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.alexandrecardoso.projetohotelfei;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Para ver em outra tela, usar:
+// import static com.example.alexandrecardoso.projetohotelfei.MainActivity.admsLogados;
+
+
 public class MainActivity extends AppCompatActivity {
+    // Cria variáveis globais (COLOCAR NA PRIMEIRA TELA CHAMADA PELO APP)
+    public static Administrador admsLogados[] = new Administrador[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Cria variáveis globais (COLOCAR NA PRIMEIRA TELA CHAMADA PELO APP)
-        Administrador admsLogados[];
         Administrador admInicial = new Administrador("admin","Administrador",123456789,"01/01/2019","admin@admin.com.br","11961839318","admin");
+        admsLogados[0] = admInicial;
+
+
 
     }
 }
