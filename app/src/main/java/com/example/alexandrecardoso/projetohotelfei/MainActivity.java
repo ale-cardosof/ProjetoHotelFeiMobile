81715b6552d9a9fa7b348b56cbb573ab68632c21
Alexandre Cardoso <alexandrecardosofeitosa@outlook.com.br>
2019-04-13 12:31:03 -0300

    Teste

--- app/src/main/java/com/example/alexandrecardoso/projetohotelfei/MainActivity.java
+++ app/src/main/java/com/example/alexandrecardoso/projetohotelfei/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.alexandrecardoso.projetohotelfei;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 // Para ver em outra tela, usar:
 // import static com.example.alexandrecardoso.projetohotelfei.MainActivity.admsLogados;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     // Cria variáveis globais (COLOCAR NA PRIMEIRA TELA CHAMADA PELO APP)
     public static Administrador admsLogados[] = new Administrador[5];
+    //public static Usuario usuariosLogados[] = new Usuario[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Administrador admInicial = new Administrador("admin","Administrador",123456789,"01/01/2019","admin@admin.com.br","11961839318","admin");
         admsLogados[0] = admInicial;
-
-
 
     }
 }
