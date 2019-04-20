63075980e96c4b97ad2a6e20267325a320365466
lucasserain <lucas.serain@outlook.com>
2019-04-20 17:54:30 -0300

    Menu Estrutura de Hotel

--- app/src/main/java/com/example/alexandrecardoso/projetohotelfei/Quarto.java
+++ app/src/main/java/com/example/alexandrecardoso/projetohotelfei/Quarto.java
@@ -7,4 +7,55 @@ public class Quarto {
     private int qntdCamas;
     private int qntdChuveiros;
     private boolean possuiTv;
+
+    public Quarto() {
+    }
+
+    public int getNumPorta() {
+        return numPorta;
+    }
+
+    public void setNumPorta(int numPorta) {
+        this.numPorta = numPorta;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public void setValorDiaria(double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getQntdCamas() {
+        return qntdCamas;
+    }
+
+    public void setQntdCamas(int qntdCamas) {
+        this.qntdCamas = qntdCamas;
+    }
+
+    public int getQntdChuveiros() {
+        return qntdChuveiros;
+    }
+
+    public void setQntdChuveiros(int qntdChuveiros) {
+        this.qntdChuveiros = qntdChuveiros;
+    }
+
+    public boolean isPossuiTv() {
+        return possuiTv;
+    }
+
+    public void setPossuiTv(boolean possuiTv) {
+        this.possuiTv = possuiTv;
+    }
 }
