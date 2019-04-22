94b0ef2311417c5d86e33e438ceb2d5c7c2d6a45
Alexandre Cardoso <alexandrecardosofeitosa@outlook.com.br>
2019-04-22 00:11:03 -0300

    Criação da tela de visualização resumida do hotel

--- app/src/main/java/com/example/alexandrecardoso/projetohotelfei/menuVisualizarHotel.java
+++ app/src/main/java/com/example/alexandrecardoso/projetohotelfei/menuVisualizarHotel.java
@@ -9,5 +9,6 @@ public class menuVisualizarHotel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_visualizar_hotel);
+        getSupportActionBar().hide();
     }
 }
